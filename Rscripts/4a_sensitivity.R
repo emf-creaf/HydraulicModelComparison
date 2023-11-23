f40e1e45fe49a82aa9eb380d8ac6a3f0713dba05
Miquel De Cáceres <miquelcaceres@gmail.com>
2023-11-23 13:15:26 +0100

    sensitivity analysis

---
+++ Rscripts/4a_sensitivity.R
@@ -0,0 +1,227 @@
+################################################################################################################################
+#   Performs sensitivity analysis of time to hidraulic failure and time to stomatal closure in sureau-medfate
+################################################################################################################################
+library(medfate)
+library(sensitivity)
+library(ggplot2)
+library(doParallel)
+library(boot)
+
+# Terrain -----------------------------------------------------------------
+pue_latitude <- 43.74139
+pue_elevation <- 270
+pue_slope <- 0
+pue_aspect <- 0
+
+
+# Common soil -------------------------------------------------------------
+common_soil <- function(){
+  fb_soil_df <- defaultSoilParams(n = 3)
+  fb_soil_df$widths <- c(200, 800, 3000)
+  fb_soil_df$rfc <- c(30, 50, 94.5)
+  fb_soil_df$clay <- rep(39, 3)
+  fb_soil_df$sand <- rep(26, 3)
+  fb_soil_df$om <- c(6,3,1)
+  fb_soil_df$bd <-rep(1.45, 3)
+  
+  fb_soil <- soil(fb_soil_df)
+  fb_soil$VG_theta_sat <- rep(0.5, 3)
+  fb_soil$VG_theta_res <- rep(0.098, 3)
+  fb_soil$VG_n <- rep(1.55, 3)
+  # 1 cm = 0.00009804139432 MPa
+  fb_soil$VG_alpha <- rep(0.0005/0.00009804139432, 3)
+  fb_soil$Ksat <- rep(655.2934*1.69, 3)
+  
+  return(fb_soil)
+}
+soil <- common_soil()
+
+
+# Initial forest ----------------------------------------------------------
+initial_forest <- function(){
+  pue_forest <- emptyforest(1)
+  pue_forest$treeData$Species <- "Quercus ilex"
+  pue_forest$treeData$DBH <- 9.11
+  pue_forest$treeData$Height <- 530.2
+  pue_forest$treeData$N <- 1750
+  pue_forest$treeData$Z50 <- 200
+  pue_forest$treeData$Z95 <- 1000
+  pue_forest$treeData$LAI <- 2.2
+  return(pue_forest)
+}
+forest <- initial_forest()
+
+# Weather -----------------------------------------------------------------
+data("examplemeteo")
+meteo <- examplemeteo
+row.names(meteo) <- NULL
+meteo <- rbind(meteo, meteo, meteo)
+meteo$dates <- seq(as.Date("2001-01-01"), as.Date("2003-12-31"), by="day")
+meteo$DOY <- 200
+meteo$JulianDay <- 200
+meteo$MinTemperature <- 20
+meteo$MaxTemperature <- 30
+meteo$MinRelativeHumidity <- 30
+meteo$MaxRelativeHumidity <- 90
+meteo$WindSpeed <- 2
+meteo$Radiation  <- 30
+meteo$Precipitation <- 0 # To force dessication
+
+# Build initial input object ----------------------------------------------
+control <- defaultControl("Cochard")
+control$subdailyResults <- FALSE
+control$cavitationRefillStem <- "none"
+control$cavitationRefillLeaves <- "none"
+control$bareSoilEvaporation <- FALSE
+control$plantCapacitance <- TRUE
+control$cavitationFlux <- TRUE
+control$sapFluidityVariation <- TRUE
+control$leafCuticularTranspiration <- TRUE
+control$stemCuticularTranspiration <- TRUE
+control$rhizosphereOverlap <- "total"
+control$stomatalSubmodel <- "Baldocchi"
+control$gs_NightFrac <- 0.05
+control$verbose <- FALSE
+
+x_initial <- forest2spwbInput(forest, soil, SpParamsMED, control)
+x_initial$canopy$Tair <- 29
+x_initial$canopy$Cair <- 386
+x_initial$canopy$VPair <- 1.688
+x_initial$soil$Temp <- c(32,29,27.71661)
+
+
+# Test parameter modification ---------------------------------------------
+tarcoh <- "T1_168"
+parNames = c("rfc@2",
+             paste0(tarcoh,c("/Z95", "/LAI_live", "/Plant_kmax", "/Gswmax", "/Vmax298", "/Gs_P50", "/Gswmin", "/VCstem_P50", "/Vsapwood")))
+customParams <- c(10,500, 0.5, 1.0, 0.2, 150, -3, 0.003, -4, 2)
+names(customParams) <- parNames
+x_mod <- modifyInputParams(x_initial, customParams)
+
+
+# Optimization functions --------------------------------------------------
+parMin <- c(10,500, 0.5, 0.5, 0.1, 100, -3.5, 0.001, -5.5, 0.1)
+parMax <- c(90,2500, 6.0, 2.0, 0.4, 200, -1.5, 0.004, -1.5, 1.0)
+
+# First day with maximum gs < 5% of gsmax in Sunlit leaves
+sf_timetoclosure<-function(x){
+  gswmax <- x$SunlitLeaves$GSWMax
+  which(gswmax < x$spwbInput$paramsTranspiration$Gswmax*0.05)[1] 
+}
+#First day with missing StemPLC, meaning it stoped before
+sf_timetofailure<-function(x){ 
+  which(is.na(x$Plants$StemPLC))[1]
+}
+# Survival time as the difference
+sf_survivaltime<-function(x){ 
+  sf_timetofailure(x) - sf_timetoclosure(x)
+}
+
+
+of_timetoclosure<-optimization_function(parNames = parNames,
+                                     x = x_initial,
+                                     meteo = meteo,
+                                     latitude = pue_latitude, elevation = pue_elevation,
+                                     slope = pue_slope, aspect = pue_aspect,
+                                     summary_function = sf_timetoclosure)
+
+of_timetofailure<-optimization_function(parNames = parNames,
+                                     x = x_initial,
+                                     meteo = meteo,
+                                     latitude = pue_latitude, elevation = pue_elevation,
+                                     slope = pue_slope, aspect = pue_aspect,
+                                     summary_function = sf_timetofailure)
+
+of_survivaltime<-optimization_function(parNames = parNames,
+                                        x = x_initial,
+                                        meteo = meteo,
+                                        latitude = pue_latitude, elevation = pue_elevation,
+                                        slope = pue_slope, aspect = pue_aspect,
+                                        summary_function = sf_survivaltime)
+
+of_timetoclosure(parMin)
+of_timetoclosure(parMax)
+of_timetofailure(parMin)
+of_timetofailure(parMax)
+of_survivaltime(parMin)
+of_survivaltime(parMax)
+
+
+# Parameter matrices ------------------------------------------------------
+n <- 10
+p <- length(parMin)
+M1 <- sweep(matrix(runif(p * n), nrow = n),2,STATS = (parMax-parMin), FUN = "*")
+M1 <- sweep(M1, 2, parMin, FUN="+")
+X1 <- data.frame(M1)
+names(X1)<-parNames
+M2 <- sweep(matrix(runif(p * n), nrow = n),2,STATS = (parMax-parMin), FUN = "*")
+M2 <- sweep(M2, 2, parMin, FUN="+")
+X2 <- data.frame(M2)
+names(X2)<-parNames
+
+# Parallelization functions --------------------------------------------------------
+ncores <- 6
+mult_timetoclosure <- function(X) {
+  cat(paste0("Entering mult_timetoclosure n = ", nrow(X), "\n"))
+  doParallel::registerDoParallel(cores = ncores)
+  r <- foreach::foreach(index = 1:nrow(X)) %dopar% {
+    v <- as.numeric(X[index,])
+    names(v) <- names(X)
+    of_timetoclosure(v, verbose = FALSE)
+  }
+  doParallel::stopImplicitCluster()
+  r<- unlist(r)
+  print(summary(r))
+  return(r)
+}
+mult_timetofailure <- function(X) {
+  cat(paste0("Entering mult_timetofailure n = ", nrow(X), "\n"))
+  doParallel::registerDoParallel(cores = ncores)
+  r <- foreach::foreach(index = 1:nrow(X)) %dopar% {
+    v <- as.numeric(X[index,])
+    names(v) <- names(X)
+    of_timetofailure(v, verbose = FALSE)
+  }
+  doParallel::stopImplicitCluster()
+  r<- unlist(r)
+  print(summary(r))
+  return(r)
+}
+mult_survivaltime <- function(X) {
+  cat(paste0("Entering mult_survivaltime n = ", nrow(X), "\n"))
+  doParallel::registerDoParallel(cores = ncores)
+  r <- foreach::foreach(index = 1:nrow(X)) %dopar% {
+    v <- as.numeric(X[index,])
+    names(v) <- names(X)
+    of_survivaltime(v, verbose = FALSE)
+  }
+  doParallel::stopImplicitCluster()
+  r<- unlist(r)
+  print(summary(r))
+  return(r)
+}
+
+mult_timetoclosure(X1[1:6,])
+mult_timetofailure(X1[1:6,])
+mult_survivaltime(X1[1:6,])
+
+# Sensitivity analyses ----------------------------------------------------
+nboot <- 0
+
+sa_salt_timetoclosure <- sobolSalt(model = mult_timetoclosure, X1, X2,
+                                   scheme="A", nboot = nboot)
+saveRDS(sa_salt_timetoclosure, "Rdata/sa_salt_timetoclosure.rds")
+print(sa_salt_timetoclosure)
+ggplot(sa_salt_timetoclosure, choice=1)
+
+sa_salt_timetofailure <- sobolSalt(model = mult_timetofailure, X1, X2,
+                                   scheme="A", nboot = nboot)
+saveRDS(sa_salt_timetofailure, "Rdata/sa_salt_timetofailure.rds")
+print(sa_salt_timetofailure)
+ggplot(sa_salt_timetofailure, choice=1)
+
+sa_salt_survivaltime <- sobolSalt(model = mult_survivaltime, X1, X2,
+                                   scheme="A", nboot = nboot)
+saveRDS(sa_salt_survivaltime, "Rdata/sa_salt_survivaltime.rds")
+print(sa_salt_survivaltime)
+ggplot(sa_salt_survivaltime, choice=1)
